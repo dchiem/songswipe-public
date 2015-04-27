e133eb257f66a53a49cecdcb44c771566d911587
JB <jbakst@stanford.edu>
2015-04-27 11:25:34 -0700

    Initial Commit

---
+++ SongSwipe/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  SongSwipe
+//
+//  Created by JB Bakst on 4/27/15.
+//  Copyright (c) 2015 JB Bakst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SongSwipe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SongSwipe
+//
+//  Created by JB Bakst on 4/27/15.
+//  Copyright (c) 2015 JB Bakst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
